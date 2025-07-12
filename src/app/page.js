7382b177475b7c6d94e648570f47b18e21e7b41b
erfan hosseini <erfanhosseini768@yahoo.com>
2025-07-12 10:20:19 +0330

    import Register(tem-compo)

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,10 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import Register from '@/components/templates/auth/register/Register';
+import styles from './page.module.css';
 
-export default function Home() {
+export default function RegisterPage() {
   return (
     <>
-    
+    <Register />
     </>
   );
 }
