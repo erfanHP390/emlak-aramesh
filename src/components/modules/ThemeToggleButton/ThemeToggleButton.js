466b55e84dc0bc812e607667776fcf876ef9ff5b
erfan hosseini <erfanhosseini768@yahoo.com>
2025-07-14 23:22:04 +0330

    create module-component theme-toggle-btn

---
+++ src/components/modules/ThemeToggleButton/ThemeToggleButton.js
@@ -0,0 +1,18 @@
+"use client";
+import { useContext } from "react";
+import { ThemeContext } from "@/context/ThemeContext";
+import { FaSun, FaMoon } from "react-icons/fa";
+import styles from "./ThemeToggleButton.module.css";
+export default function ToggleThemeButton() {
+  const { theme, toggleTheme } = useContext(ThemeContext);
+
+  return (
+    <button
+      className={styles.toggle_btn}
+      onClick={toggleTheme}
+      aria-label="تغییر تم"
+    >
+      {theme === "dark" ? <FaSun size={24} /> : <FaMoon size={24} />}
+    </button>
+  );
+}
