0958be9eb08ccca9953816a25f70708a7ef53e34
erfan hosseini <erfanhosseini768@yahoo.com>
2025-07-15 16:42:53 +0330

    create page forgot-password-user

---
+++ src/app/forgotPassword/page.js
@@ -0,0 +1,12 @@
+import ForgotPass from '@/components/templates/auth/forgotPass/ForgotPass'
+import React from 'react'
+
+function page() {
+  return (
+    <>
+      <ForgotPass />
+    </>
+  )
+}
+
+export default page
