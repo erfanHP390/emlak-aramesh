724b7ed18f97608f742af9aa4f3821a2652e221f
erfan hosseini <erfanhosseini768@yahoo.com>
2025-07-15 19:52:58 +0330

    create page lock-screen-page

---
+++ src/app/lockPage/page.js
@@ -0,0 +1,12 @@
+import LockScreen from '@/components/templates/auth/lockScreen/lockScreen'
+import React from 'react'
+
+function page() {
+  return (
+    <>
+      <LockScreen />
+    </>
+  )
+}
+
+export default page
