dd1fd301673e2ef74a8bbf7c260cf548ad86b3e3
erfan hosseini <erfanhosseini768@yahoo.com>
2025-07-15 10:37:54 +0330

    create page login

---
+++ src/app/login/page.js
@@ -0,0 +1,13 @@
+import Login from '@/components/templates/auth/login/Login'
+import React from 'react'
+
+async function page() {
+
+  return (
+    <>
+      <Login />
+    </>
+  )
+}
+
+export default page
