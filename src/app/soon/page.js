284bfb551d8924b323e9678a5a8bb7a546962635
erfan hosseini <erfanhosseini768@yahoo.com>
2025-07-16 08:58:20 +0330

    create page soon

---
+++ src/app/soon/page.js
@@ -0,0 +1,12 @@
+import ComingSoon from '@/components/templates/ComingSoon'
+import React from 'react'
+
+function page() {
+  return (
+    <>
+      <ComingSoon />
+    </>
+  )
+}
+
+export default page
