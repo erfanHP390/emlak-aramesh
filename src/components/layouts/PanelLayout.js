21436462c92cf8b3f8c231d49e55b45150977348
erfan hosseini <erfanhosseini768@yahoo.com>
2025-07-18 22:46:32 +0330

    add info user from data-base

--- src/components/layouts/PanelLayout.js
+++ src/components/layouts/PanelLayout.js
@@ -2,12 +2,18 @@ import React from "react";
 import Sidebar from "../modules/sidebar/Sidebar";
 import Topbar from "../modules/topbar/Topbar";
 import styles from "./PanelLayout.module.css";
+import connectToDB from "@/configs/db";
+import { authUser } from "@/utils/authUser";
 
-function PanelLayout({ children }) {
+async function PanelLayout({ children }) {
+
+  connectToDB()
+  const user = await authUser()
+
   return (
     <div className={styles.layoutContainer}>
       <Topbar />
-      <Sidebar />
+      <Sidebar  user={user} />
       <main className={styles.mainContent}>
         {children}
       </main>
