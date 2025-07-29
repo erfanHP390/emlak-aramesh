f03ba1dd1d838e8d94b1a2415f91e06d0ea7f41a
erfan hosseini <erfanhosseini768@yahoo.com>
2025-07-29 20:45:28 +0330

    create page house-details

---
+++ src/app/houseDetails/[id]/page.js
@@ -0,0 +1,13 @@
+import PanelLayout from '@/components/layouts/PanelLayout'
+import HouseDetails from '@/components/templates/houseDetails/HouseDetails'
+import React from 'react'
+
+function page() {
+  return (
+    <PanelLayout>
+      <HouseDetails />
+    </PanelLayout>
+  )
+}
+
+export default page
