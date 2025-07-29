74343ff688d3c2b7f160bd82385c14330486e89a
erfan hosseini <erfanhosseini768@yahoo.com>
2025-07-29 20:46:51 +0330

    connection to db and get info-houses

--- src/app/myHouses/page.js
+++ src/app/myHouses/page.js
@@ -1,16 +1,33 @@
 import PanelLayout from "@/components/layouts/PanelLayout";
 import MyRegistrationHouse from "@/components/templates/myRegistrationHouse/MyRegistrationHouse";
 import connectToDB from "@/configs/db";
-import React from "react";
+import HouseModel from "@/models/House";
+import ConsultantModel from "@/models/Consultant";
+import { authUser } from "@/utils/authUser";
+import { redirect } from "next/navigation";
 
 async function page() {
+  await connectToDB();
 
+  const user = await authUser();
 
-    connectToDB()
+  if (!user) {
+    redirect("/login")
+  }
+
+  const consultant = await ConsultantModel.findOne({ user: user._id });
+
+  if (!consultant) {
+    redirect("/dashboard")
+  }
+
+  const houses = await HouseModel.find({ consultant: consultant._id }).lean();
+
+  
 
   return (
     <PanelLayout>
-      <MyRegistrationHouse />
+      <MyRegistrationHouse houses={JSON.parse(JSON.stringify(houses))} />
     </PanelLayout>
   );
 }
