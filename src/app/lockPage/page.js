7be27c76a708fb4383f2510a65331ed7d8d6b50f
erfan hosseini <erfanhosseini768@yahoo.com>
2025-07-29 22:57:39 +0330

    get consultant-info

--- src/app/lockPage/page.js
+++ src/app/lockPage/page.js
@@ -1,18 +1,20 @@
-import LockScreen from '@/components/templates/auth/lockScreen/lockScreen'
-import connectToDB from '@/configs/db'
-import { authUser } from '@/utils/authUser'
-import React from 'react'
+import LockScreen from "@/components/templates/auth/lockScreen/lockScreen";
+import connectToDB from "@/configs/db";
+import { authUser } from "@/utils/authUser";
+import React from "react";
+import UserModel from "@/models/User";
+import ConsultantModel from "@/models/Consultant";
 
 async function page() {
-
-  connectToDB()
-  const user = await authUser()
+  connectToDB();
+  const user = await authUser();
+  const consultant = await ConsultantModel.findOne({ email : user.email})
 
   return (
     <>
-      <LockScreen user={user} />
+      <LockScreen user={user} consultant={consultant} />
     </>
-  )
+  );
 }
 
-export default page
+export default page;
