a9db2a50ec812846da1bda21ac7a043ce83f8f68
erfan hosseini <erfanhosseini768@yahoo.com>
2025-07-30 23:26:14 +0330

    page-protection and get houses-info from DB

--- src/app/houseList/page.js
+++ src/app/houseList/page.js
@@ -1,11 +1,30 @@
 import PanelLayout from "@/components/layouts/PanelLayout";
 import HomesList from "@/components/templates/homesList/HomesList";
 import React from "react";
+import connectToDB from "@/configs/db";
+import HouseModel from "@/models/House";
+import ConsultantModel from "@/models/Consultant";
+import { authUser } from "@/utils/authUser";
+import { redirect } from "next/navigation";
 
-function page() {
+
+async function page() {
+
+   await connectToDB();
+
+  const user = await authUser();
+
+  if (!user) {
+    redirect("/login")
+  }
+
+  const consultant = await ConsultantModel.findOne({ user: user._id });
+  const houses = await HouseModel.find().populate("consultant").lean();
+
+
   return (
     <PanelLayout>
-    <HomesList />
+    <HomesList  houses={JSON.parse(JSON.stringify(houses))}   />
     </PanelLayout>
   );
 }
