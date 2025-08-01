7e6be609904d01289b39006a477388631b07ff21
erfan hosseini <erfanhosseini768@yahoo.com>
2025-08-01 21:44:13 +0330

    get clients/houses for consultant from DB

--- src/app/houseDetails/[id]/page.js
+++ src/app/houseDetails/[id]/page.js
@@ -3,12 +3,29 @@ import HouseDetails from "@/components/templates/houseDetails/HouseDetails";
 import connectToDB from "@/configs/db";
 import React from "react";
 import HouseModel from "@/models/House";
+import ConsultantModel from "@/models/Consultant";
+import ClientModel from "@/models/Client"
 
 async function page({ params }) {
   connectToDB();
-  const house = await HouseModel.findOne({ _id: params.id })
-    .populate("consultant")
-    .lean();
+const house = await HouseModel.findOne({ _id: params.id })
+  .populate({
+    path: "consultant",
+    model: ConsultantModel,
+    select: "-password",
+    populate: [
+      {
+        path: "clients",
+        model: "Client",
+      },
+      {
+        path: "houses",
+        model: "House",
+      },
+    ],
+  })
+  .lean();
+
 
   return (
     <PanelLayout>
