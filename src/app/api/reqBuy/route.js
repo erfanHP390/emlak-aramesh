6aee166c374fc64bb2a227c2aa927108725bdc7a
erfan hosseini <erfanhosseini768@yahoo.com>
2025-08-02 18:57:45 +0330

    create route with method post for add request-for-houses

---
+++ src/app/api/reqBuy/route.js
@@ -0,0 +1,75 @@
+import connectToDB from "@/configs/db";
+import { validateEmail, validatePhone } from "@/utils/auth";
+import ConsultantModel from "@/models/Consultant"
+import HouseModel from "@/models/House"
+import ReqBuyModel from "@/models/ReqBuy"
+
+export async function POST(req) {
+  try {
+    connectToDB();
+
+    const body = await req.json();
+    const { name, phone, email, description, codeConsultant, codeHouse } = body;
+
+    if (
+      !name ||
+      !phone ||
+      !email ||
+      !description ||
+      !codeConsultant ||
+      !codeHouse
+    ) {
+      return Response.json(
+        { message: "All information must be sent." },
+        {
+          status: 400,
+        }
+      );
+    }
+
+    const isValidPhone = validatePhone(phone);
+    if (!isValidPhone) {
+      return Response.json(
+        { message: "phone is not valid" },
+        {
+          status: 422,
+        }
+      );
+    }
+
+    const isValidEmail = validateEmail(email);
+    if (!isValidEmail) {
+      return Response.json(
+        { message: "email is not valid" },
+        {
+          status: 422,
+        }
+      );
+    }
+
+    const isExistConsultant = await ConsultantModel.findOne({hisCode: codeConsultant})
+    const isExistHouse = await HouseModel.findOne({codeHouse})
+
+    await ReqBuyModel.create({
+        name,
+        phone,
+        email,
+        description,
+        consultant: isExistConsultant ? isExistConsultant._id : null,
+        houses: isExistHouse ? [isExistHouse._id] : []
+    })
+
+
+    return Response.json({message: "request buy house is created successfully"} , {
+        status: 201
+    })
+
+  } catch (err) {
+    return Response.json(
+      { message: `interval error server : ${err}` },
+      {
+        status: 500,
+      }
+    );
+  }
+}
