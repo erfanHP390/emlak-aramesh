b6345030c300b8d7dc63174fb1e12eb036dee458
erfan hosseini <erfanhosseini768@yahoo.com>
2025-08-04 01:35:47 +0330

    create page add-consultant

---
+++ src/app/addConsultant/page.js
@@ -0,0 +1,13 @@
+import PanelLayout from '@/components/layouts/PanelLayout'
+import AddConsultantForm from '@/components/templates/addConsultant/AddConsultantForm'
+import React from 'react'
+
+function page() {
+  return (
+    <PanelLayout>
+        <AddConsultantForm />
+    </PanelLayout>
+  )
+}
+
+export default page
