d3fa2eebfd4b307a0936558ae37ed409bedf46df
erfan hosseini <erfanhosseini768@yahoo.com>
2025-08-07 10:16:52 +0330

    create page contact

---
+++ src/app/contact/page.js
@@ -0,0 +1,13 @@
+import PanelLayout from '@/components/layouts/PanelLayout'
+import ContactForm from '@/components/templates/contactForm/ContactForm'
+import React from 'react'
+
+function page() {
+  return (
+    <PanelLayout>
+      <ContactForm />
+    </PanelLayout>
+  )
+}
+
+export default page
