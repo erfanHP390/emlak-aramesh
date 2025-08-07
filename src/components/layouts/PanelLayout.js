2f48a33e5c7be293cbdea59254d7db585abae288
erfan hosseini <erfanhosseini768@yahoo.com>
2025-08-07 09:40:53 +0330

    import wrapper (sidebar/topbar) to panel

--- src/components/layouts/PanelLayout.js
+++ src/components/layouts/PanelLayout.js
@@ -1,27 +1,28 @@
 import React from "react";
-import Sidebar from "../modules/sidebar/Sidebar";
-import Topbar from "../modules/topbar/Topbar";
+
 import styles from "./PanelLayout.module.css";
 import connectToDB from "@/configs/db";
 import { authAdmin, authConsultant, authUser } from "@/utils/authUser";
-import UserModel from "@/models/User"
-import ConsultantModel from "@/models/Consultant"
+import UserModel from "@/models/User";
+import ConsultantModel from "@/models/Consultant";
+import Wrapper from "./Wrapper";
 
 async function PanelLayout({ children }) {
-
-  connectToDB()
-  const user = await authUser()
-  const consultant = await authConsultant()
-  const admin = await authAdmin()
-  const consultantInfo = await ConsultantModel.findOne({user: user._id})
+  connectToDB();
+  const user = await authUser();
+  const consultant = await authConsultant();
+  const admin = await authAdmin();
+  const consultantInfo = await ConsultantModel.findOne({ user: user._id });
 
   return (
     <div className={styles.layoutContainer}>
-      <Topbar />
-      <Sidebar  user={user}  consultant={consultant} consultantInfo={consultantInfo}  admin={admin} />
-      <main className={styles.mainContent}>
-        {children}
-      </main>
+      <Wrapper
+        user={user}
+        consultant={consultant}
+        consultantInfo={consultantInfo}
+        admin={admin}
+      >{children}</Wrapper>
+      {/* <main className={styles.mainContent}>{children}</main> */}
     </div>
   );
 }
