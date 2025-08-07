312a41699fc64c003d929956b1c530536412e937
erfan hosseini <erfanhosseini768@yahoo.com>
2025-08-07 09:41:44 +0330

    create layout wrapper

---
+++ src/components/layouts/Wrapper.js
@@ -0,0 +1,38 @@
+"use client";
+import React, { useState } from "react";
+import Topbar from "../modules/topbar/Topbar";
+import Sidebar from "../modules/sidebar/Sidebar";
+import styles from "./PanelLayout.module.css";
+
+function Wrapper({ user, consultant, consultantInfo, admin, children }) {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+
+  return (
+    <>
+      <Sidebar
+        user={user}
+        consultant={consultant}
+        consultantInfo={consultantInfo}
+        admin={admin}
+        isOpen={isSidebarOpen}
+        toggleSidebar={toggleSidebar}
+      />
+      <div className={`${styles.mainContent} ${!isSidebarOpen ? styles.fullWidth : ''}`}>
+        <Topbar
+          user={user}
+          consultant={consultant}
+          consultantInfo={consultantInfo}
+          admin={admin}
+          toggleSidebar={toggleSidebar}
+        />
+        {children}
+      </div>
+    </>
+  );
+}
+
+export default Wrapper;
