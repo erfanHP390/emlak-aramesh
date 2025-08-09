1bfe156f251f2c25a1b2fe819707cf31e0398307
erfan hosseini <erfanhosseini768@yahoo.com>
2025-08-09 11:16:20 +0330

    create general  ask&answer for users

---
+++ src/app/faq/page.js
@@ -0,0 +1,13 @@
+import PanelLayout from '@/components/layouts/PanelLayout'
+import MainFaq from '@/components/templates/mainFaq/MainFaq'
+import React from 'react'
+
+function page() {
+  return (
+    <PanelLayout>
+      <MainFaq />
+    </PanelLayout>
+  )
+}
+
+export default page
