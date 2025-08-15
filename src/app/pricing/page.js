e1125f3acf55c2c1c3c8175fdb7a756e36f34cf0
erfan hosseini <erfanhosseini768@yahoo.com>
2025-08-15 23:51:43 +0330

    create page pricing plans-site

---
+++ src/app/pricing/page.js
@@ -0,0 +1,13 @@
+import PanelLayout from '@/components/layouts/PanelLayout'
+import MainPricing from '@/components/templates/pricing/MainPricing'
+import React from 'react'
+
+function page() {
+  return (
+    <PanelLayout>
+      <MainPricing />
+    </PanelLayout>
+  )
+}
+
+export default page
