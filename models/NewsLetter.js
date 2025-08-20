45cf1fad261e70bed04f3a3d08ca0c10977c7e3a
erfan hosseini <erfanhosseini768@yahoo.com>
2025-08-20 10:10:51 +0330

    create model news-letter

---
+++ models/NewsLetter.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const schema = mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const model = mongoose.models.NewsLetter || mongoose.model("NewsLetter", schema);
+export default model;
