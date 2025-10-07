251233e47c521768d52180cef27dc4f52f5dc2f2
erfan hosseini <erfanhosseini768@yahoo.com>
2025-10-07 21:20:32 +0330

    create model notification

--- models/Notification.js
+++ models/Notification.js
@@ -2,16 +2,19 @@ import mongoose from "mongoose";
 
 const notificationSchema = new mongoose.Schema(
   {
-    message: {
-      type: String,
-      required: true,
-    },
+    text: { type: String, required: true },
     type: {
       type: String,
-      enum: ["create", "update", "delete"],
-      required: true,
+      enum: ["info", "success", "warning", "danger"],
+      default: "info",
     },
-    read: { type: Boolean, default: false },
+    link: { type: String, default: "#" },
+    icon: {
+      type: String,
+      enum: ["users", "shopping", "warning", "task"],
+      default: "info",
+    },
+    isRead: { type: Boolean, default: false },
   },
   { timestamps: true }
 );
